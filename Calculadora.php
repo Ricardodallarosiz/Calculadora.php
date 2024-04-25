a01099485e1ab9e107330dbdb6365094ec83b674
Ricardodallarosiz <dallarosizr@gmail.com>
2024-04-24 23:37:03 -0300

    segundo commit

--- Calculadora.php
+++ Calculadora.php
@@ -1,3 +1,25 @@
-<?php
-echo "a calculadora vai dar boa!";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora.php</title>
+</head>
+<body>
+    <form>
+        Primeiro Número <br />
+        <input type="numero" name="numero1"/><br />
+        Segundo Número <br />
+        <input type="numero" name="numero2"/><br /><br />
+        <select name ="calcular">
+            <option value ="somar">Somar</option>
+            <option value ="subtrair">Subtrair</option>
+            <option value ="multiplicar">Multiplicar</option>
+            <option value ="dividir">Dividir</option>
+        </select>
+    <br /><br />
+    <input type="submit" value="Calcular"/>
+    </form>
+    
+</body>
+</html>
